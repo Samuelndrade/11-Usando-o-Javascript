9d9f3865084d49fb1122e75f53ecc281cb09d39a
Samuel Juneo de Andrade Costa <132668928+Samuelndrade@users.noreply.github.com>
2024-09-24 18:59:51 +0000

    Modifiquei meu texto

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-<script>
 console.log('Olá mundo!');
-</script>
+// Criando um novo elemento 
+let novoElemento = document.createElement('h1');
+// Alterando o conteúdo de texto do elemento
+novoElemento.innerText = 'Hello, World! English! (Inglês) Ok?!';
+// Selecionando o elemento body
+let elementoBody = document.body;
+// Colocando o novo elemento no body
+elementoBody.appendChild(novoElemento);
